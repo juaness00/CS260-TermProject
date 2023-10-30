d2771f0e85f97206e52f20eab5af0161e0f9c05f
Emanuel Espitia <eespiti@jacksonville.edu>
2023-10-30 14:02:17 -0400

    gay

--- customer.cpp
+++ customer.cpp
@@ -1,4 +1,16 @@
   #include "customer.h"
- void Customer::setName(string _name){
-    name = _name;
+ 
+ 
+ 
+ 
+ 
+ void Customer::setName(string _fname, string _lname, string _adress, string _phone, string _email)
+  {
+    fname = _fname;
+    lname = _lname;
+    adress = _adress;
+    phone = _phone;
+    email = _email;
+
   }
+
--- customer.h
+++ customer.h
@@ -5,10 +5,14 @@
 #include <string>
 using namespace std;
 
-class Customer{
-  string name;
-  void setName(string _name);
+class Customer
+{
+  string fname, lname, adress, phone, email;
 
+  public:
+  Customer(string _fname, string _lname, string _adress, string _phone, string _email);
+  void setName(string _fname, string _lname, string _adress, string _phone, string _email);
+  
 };
 
 
