8fc6ca1d744817abf0895f2467ddaf39530469e1
juaness00 <juanessanchez01@gmail.com>
2023-11-03 09:38:34 -0400

    added more Account class methods

--- account.cpp
+++ account.cpp
@@ -1,20 +1,17 @@
 #include "account.h"
 
 
-Account::Account(){
+Account::Account(): accountCustomer(){
     balance = 0;
     withdraws = 0;
     deposits = 0;
     
 }
 
-Account::Account(string _fName, string _lName, string _address, string _phone, string _email, int _id, double _balance)
+Account::Account(string _fName, string _lName, string _address, string _phone, string _email, int _id, double _balance): accountCustomer(_fName, _lName,_address,_phone,_email)
 {
-    accountCustomer.setAll(_fName, _lName, _address, _phone, _email);
     id = _id;
     balance = _balance;
-    
-
 };
 
 void Account::deposit(double _amount)
@@ -37,7 +34,9 @@ void Account::withdraw(double _amount)
 }
 void Account::printInfo()
 {
+    
     cout << "Displaying information for account #" << id << endl;
+    accountCustomer.PrintInfo();
     cout << "Balance: " << balance << endl;
     cout << "Number of Withdraws: " << withdraws << endl;
     cout << "Number of Deposits: " << deposits << endl;
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "account.h"
 using namespace std;
 
 int main()
